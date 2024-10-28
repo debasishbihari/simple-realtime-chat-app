0777773171ed0b83006430de3128692313a9024c
Debasish Bihari <debasisbihari287@gmail.com>
2024-10-28 05:51:10 +0530

    simple-realtime-chat-app

---
+++ src/config/firebase.js
@@ -0,0 +1,96 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import {
+  collection,
+  doc,
+  getDocs,
+  getFirestore,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjaVFOY8fNGMPdU0GyKiGnEkvOJKXdaTo",
+  authDomain: "simple-chat-app-4566a.firebaseapp.com",
+  projectId: "simple-chat-app-4566a",
+  storageBucket: "simple-chat-app-4566a.appspot.com",
+  messagingSenderId: "12116734427",
+  appId: "1:12116734427:web:5b792bc3fedb03a721f7fe",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await setDoc(doc(db, "users", user.uid), {
+      id: user.uid,
+      username: username.toLowerCase(),
+      email,
+      name: "",
+      avatar: "",
+      bio: "Hey, I am using chat app",
+      lastSeen: Date.now(),
+    });
+    await setDoc(doc(db, "chats", user.uid), {
+      chatsData: [],
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const logout = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const resetPass = async (email) => {
+  if (!email) {
+    toast.error("Please enter your email address");
+    return null;
+  }
+  try {
+    const userRef = collection(db, "users");
+    const q = query(userRef, where("email", "==", email));
+    const querySnap = await getDocs(q);
+    if (querySnap.empty) {
+      toast.error("No user found with this email address");
+      return null;
+    }
+    await sendPasswordResetEmail(auth, email);
+    toast.success("Password reset email sent successfully");
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+export { signup, login, logout, auth, db,resetPass };
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,82 @@
+import "./Login.css";
+import assets from "../../assets/assets";
+import { useState } from "react";
+import { signup,login,resetPass } from "../../config/firebase";
+
+const Login = () => {
+  const [currentState, setCurrentState] = useState("Sign Up");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if(currentState === "Sign Up") {
+      signup(userName,email,password);
+    }
+    else{
+      login(email,password);
+    }
+  };
+
+  return (
+    <div className="login">
+      <img src={assets.logo_big} className="logo" alt="" />
+      <form onSubmit={onSubmitHandler} className="login-form">
+        <h2>{currentState}</h2>
+        {currentState === "Sign Up" ? (
+          <input
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+            type="text"
+            placeholder="username"
+            className="form-input"
+            required
+          />
+        ) : null}
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          placeholder="Email Address"
+          className="form-input"
+          required
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          placeholder="Password"
+          className="form-input"
+          required
+        />
+        <button type="submit">
+          {currentState === "Sign Up" ? "Create Account" : "Login Now"}
+        </button>
+        <div className="login-term">
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+        <div className="login-forgot">
+          {currentState === "Sign Up" ? (
+            <p className="login-toggle">
+              Already have an account{" "}
+              <span onClick={() => setCurrentState("Login")}>Login here</span>
+            </p>
+          ) : (
+            <p className="login-toggle">
+              Create an account{" "}
+              <span onClick={() => setCurrentState("Sign Up")}>click here</span>
+            </p>
+          )}
+          {currentState === "Login" ? <p className="login-toggle">
+              Forgot password{" "}
+              <span onClick={() => resetPass(email)}>reset here</span>
+            </p>:null}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
